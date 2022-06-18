ead27c4ab4c02e88c688cfe8afe26205d4c74a07
Fernando Tello Porras <fernandotelloporras@gmail.com>
2022-06-17 20:26:53 -0500

    Subiendo imágenes

--- src/controllers/image.js
+++ src/controllers/image.js
@@ -1,11 +1,26 @@
+const path = require('path');
+const { randomNumber } = require('../helpers/libs');
+const fs = require('fs-extra');
+
 const ctrl = {};
 
 ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = (req, res) => {
-    
+ctrl.create = async (req, res) => {
+    //console.log(req.file);
+    const imgUrl = randomNumber();
+    console.log(imgUrl);
+    const imageTempPath = req.file.path;
+    const ext = path.extname(req.file.originalname).toLowerCase();
+    const targetPath = path.resolve('src/public/upload/${imgUrl}${ext}')
+
+    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+        await fs.rename(imageTempPath, targetPath);
+    }
+
+    res.send('Recibido!');
 };
 
 ctrl.like = (req, res) => {
