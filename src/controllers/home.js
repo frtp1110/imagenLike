e23b51ad37299f0c18fb1c5ec8566f48c51cf447
Fernando Tello Porras <fernando.tello@unmsm.edu.pe>
2022-06-22 19:57:08 -0500

    Renderizando las imágenes

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,11 @@
 const ctrl = {};
 
-ctrl.index = (req, res) => {
-    res.render('index');
+const { Image } = require('../models');
+
+ctrl.index = async (req, res) => {
+    //Ordena las imágenes por medio de la fechas (timestamp)
+    const images = await Image.find().sort({timestamp: -1}); 
+    res.render('index', {images});
 };
 
 module.exports = ctrl;
--- src/controllers/image.js
+++ src/controllers/image.js
@@ -10,25 +10,43 @@ ctrl.index = (req, res) => {
 
 };
 
-ctrl.create = async (req, res) => {
+ctrl.create = (req, res) => {
     //console.log(req.file);
-    const imgUrl = randomNumber();
-    console.log(imgUrl);
-    const imageTempPath = req.file.path;
-    const ext = path.extname(req.file.originalname).toLowerCase();
-    const targetPath = path.resolve('src/public/upload/' + imgUrl + ext)
+    
+    const saveImage = async () => {
+        const imgUrl = randomNumber();
 
-    if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
-        await fs.rename(imageTempPath, targetPath);
-        const newImg = new Image({
-            title: req.body.title,
-            filename: imgUrl + ext,
-            description: req.body.description
-        });
-        console.log(newImg)
-    }
+        //Validación en caso repita el nombre de una imagen que ya esta registrada
+        const images = await Image.find({filename: imgUrl + ext});
+    
+        if(images.length > 0){
+            saveImage();
+        }else{
+            console.log(imgUrl);
+            const imageTempPath = req.file.path;
+            const ext = path.extname(req.file.originalname).toLowerCase();
+            const targetPath = path.resolve('src/public/upload/' + imgUrl)
 
-    res.send('Recibido!');
+            if(ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif'){
+                await fs.rename(imageTempPath, targetPath);
+                const newImg = new Image({
+                    title: req.body.title,
+                    filename: imgUrl + ext,
+                    description: req.body.description
+                });
+                const imageSaved = await newImg.save();
+                //res.redirect('/images');
+                res.send('Recibido!');
+                //console.log(newImg)
+            }else{
+                //No permite que los archivos que no sean los del formato de imagen se suban al servidor
+                await fs.unlink(imageTempPath);
+                res.status(500).json({error: 'Solo se permiten imágenes'});
+            }
+        }
+    };
+
+    saveImage();
 };
 
 ctrl.like = (req, res) => {
