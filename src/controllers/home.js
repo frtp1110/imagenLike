6897c866838b77664145dd6f480953db0e55065d
Fernando Tello Porras <fernando.tello@unmsm.edu.pe>
2022-06-28 20:05:52 -0500

    Añadiendo el modelo de imágenes del sidebar

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -2,10 +2,15 @@ const ctrl = {};
 
 const { Image } = require('../models');
 
+const sidebar = require('../helpers/sidebar');
+
 ctrl.index = async (req, res) => {
     //Ordena las imágenes por medio de la fechas (timestamp)
     const images = await Image.find().sort({timestamp: -1}).lean(); 
-    res.render('index', {images});
+    let viewModel = {images: []};
+    viewModel.images = images;
+    viewModel = await sidebar(viewModel);
+    res.render('index', viewModel);
 };
 
 module.exports = ctrl;
