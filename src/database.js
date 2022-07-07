ed5eaa5b99466c6d6b093170e64ece0423928be8
Fernando Tello Porras <fernando.tello@unmsm.edu.pe>
2022-07-06 19:16:23 -0500

    Modificando diseño de los estilos

--- src/database.js
+++ src/database.js
@@ -3,14 +3,9 @@ const mongoose = require('mongoose');
 
 const {database} = require('./keys');
 
-/*mongoose.connect(database_proyecto.URI)
-    .then(db => console.log('Database is connected'))
-    .catch(err => console.error(err));
-
-*/
-
 mongoose.connect(database.URI, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true  
 })
     .then(db => console.log('Se estableció conexión con la base de datos'))
     .catch(err => console.error(err));
